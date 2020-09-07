a5b8ee490c629a60d8f8eb24119fca8db3621125
Kris <kristapsk@gmail.com>
2020-09-07 11:42:13 +0100

    center app

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,13 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 500px;
+  margin: auto;
 }
 
 #nav {
