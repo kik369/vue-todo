0256df04900c4545ea80558b16dfad9c8a9b8937
Kris <kristapsk@gmail.com>
2020-09-07 11:27:20 +0100

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in libero voluptates explicabo hic laboriosam neque, quidem minima ad perspiciatis eaque modi est, error quisquam, ipsa ullam ratione quia vel!</h1>
+  </div>
+</template>
